58bfc8bfe8de2bea19fcb66896f654d9936a943f
Roy Rooseboom <brrooseboom@outlook.com>
2022-08-20 21:18:31 +0200

    Revert "Changes to InsertItems.cs"
    This reverts commit b03b661593a7d47c3a9c58414eb4928e6f16ae7b.

--- Warehouse Program/Warehouse Program/InsertItems.cs
+++ Warehouse Program/Warehouse Program/InsertItems.cs
@@ -13,37 +13,36 @@ namespace Warehouse_Program
         /// <summary>
         /// Inserts items into the database.
         /// </summary>
-        internal void InsertItemsDB(string itemName, int itemAmount, string partNumber)
+        internal DataTable InsertItemsDB(string itemName, int itemAmount, string partNumber)
         {
             try
             {
-                // Using the 'using' statement here to close the connection  automatically
                 using (SqlConnection connection = new SqlConnection(dB.connectionString))
                 {
-                    // Query that goes into the database
-                    var query = $"INSERT INTO Stock VALUES (@itemName, @itemAmount, @partNumber)";
+                    DataTable dt = new DataTable();
+
+                    var query = $"INSERT INTO Stock (Name, Amount, Part Number) VALUES ({itemName}, {itemAmount}, {partNumber})";
 
                     SqlCommand command = new SqlCommand(query, connection);
 
-                    // Adds the value's to the query string.
-                    command.Parameters.AddWithValue("@itemName", itemName);
-                    command.Parameters.AddWithValue("@itemAmount", itemAmount);
-                    command.Parameters.AddWithValue("@partNumber", (object)partNumber ?? DBNull.Value);
-                    // Allows the above partNumber to be Null,
-                    // however we need to make the variable an object in order to work with DBNull.
+                    connection.Open();
+
+                    command.ExecuteNonQuery();
+
+                    SqlDataReader dReader = command.ExecuteReader();
+
+                    dt.Load(dReader);
 
 
+                    command.Dispose();
 
-                    connection.Open();  // Opens the connection to database
-
-                    command.ExecuteNonQuery();  // Executes the query
-
-                    command.Dispose();  // Dispose of resources we don't need anymore
+                    return dt;
                 }
             }
             catch (Exception e)
             {
                 MessageBox.Show("An Excepption has occurred!" + " Source: " + e.Message);
+                return null;
             }
 
         }
